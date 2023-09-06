38d382b2ced5062a35c29ac95f7811a5ac952127
Patrick Mallqui <agustin.pmall@gmail.com>
2023-09-05 20:03:05 -0500

    Structure of my webapp
    Creating my webapp structure

---
+++ webappdesign/Webapp.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Webapp.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ webappdesign/Webapp.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Webapp.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
